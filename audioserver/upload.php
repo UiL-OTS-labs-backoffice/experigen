ea60eef1efc6ad0215b11968ecc216b3e116a9b6
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2020-11-30 13:56:25 +0100

    Beefed up security on the audioserver

--- audioserver/upload.php
+++ audioserver/upload.php
@@ -4,12 +4,15 @@ header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-con
 // Comment out the line below right after data collection
 header("Access-Control-Allow-Origin: https://sdb.phonologist.org");
 
+const SAVE_DIR = null;
+
 
 print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
 $size = $_FILES['audio_data']['size']; //the size in bytes
 $input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
-$output = "files/".$_FILES['audio_data']['name']; //letting the client control the filename is a rather bad idea
+$name = $_FILES['audio_data']['name'];
+// Use basename to make sure we only have a filename. Prevents pesky ../../ attacks
+$name = basename($name);
 
-//move the file from temp name to local folder using $output name
-move_uploaded_file($input, $output)
-?>
+$output = SAVE_DIR . $name;
+move_uploaded_file($input, $output);
